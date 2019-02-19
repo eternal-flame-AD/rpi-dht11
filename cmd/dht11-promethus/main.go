aa6868a416b32ae0dec2ab9aefd76bd203d2a50a
eternal-flame-AD <ef@eternalflame.info>
2019-02-19 18:46:36 +0800

    collect once before start

--- cmd/dht11-promethus/main.go
+++ cmd/dht11-promethus/main.go
@@ -39,26 +39,31 @@ var humidGauge = prometheus.NewGauge(prometheus.GaugeOpts{
 	Help:      "Humidity data collected from dht11 on RPI",
 })
 
+func collect(gpio *wiringpi.GPIO) {
+	for retry := 0; retry < 3; retry++ {
+		h, t, err := dht11.Read(gpio, pinNum)
+		if err == nil {
+			tempGauge.Set(t)
+			humidGauge.Set(h)
+			break
+		} else {
+			log.Println(err)
+			time.Sleep(2 * time.Second)
+		}
+	}
+}
+
 func main() {
 	gpio, err := wiringpi.Setup(wiringpi.WiringPiSetup)
 	if err != nil {
 		panic(err)
 	}
 
+	collect(gpio)
 	go func() {
 		ticker := time.NewTicker(10 * time.Second)
 		for range ticker.C {
-			for retry := 0; retry < 3; retry++ {
-				h, t, err := dht11.Read(gpio, pinNum)
-				if err == nil {
-					tempGauge.Set(t)
-					humidGauge.Set(h)
-					break
-				} else {
-					log.Println(err)
-					time.Sleep(2 * time.Second)
-				}
-			}
+			collect(gpio)
 		}
 	}()
 
